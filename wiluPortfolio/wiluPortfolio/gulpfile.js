b7611673989ef2ff229c3556aac46cd524cb75f9
wolframowsky <wolframowsky@gmail.com>
2019-08-15 18:02:27 +0200

    Added gulpfile. Navbar in progress

---
+++ wiluPortfolio/wiluPortfolio/gulpfile.js
@@ -0,0 +1,16 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('build-css', function () {
+    return gulp
+        .src('./SASS/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./Content'));
+});
+
+gulp.task('default', ['build-css']);
