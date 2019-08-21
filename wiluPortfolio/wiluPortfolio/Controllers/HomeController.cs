d30ae8f59b15f9ef80974dff3ed9aaa22ddf7a93
wolframowsky <wolframowsky@gmail.com>
2019-08-21 11:31:15 +0200

    Projects view finished

--- wiluPortfolio/wiluPortfolio/Controllers/HomeController.cs
+++ wiluPortfolio/wiluPortfolio/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace wiluPortfolio.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Projects()
         {
             return View();
         }
